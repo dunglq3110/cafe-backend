94be39f792fb622591459d6960623745ddd70e8a
dunglq3110 <144051000+dunglq3110@users.noreply.github.com>
2024-05-19 16:14:50 +0700

    full api for ReportAPI

--- src/main/java/com/example/repository/ReceiptRepository.java
+++ src/main/java/com/example/repository/ReceiptRepository.java
@@ -7,15 +7,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 public interface ReceiptRepository extends JpaRepository<Receipt, Long> {
     Receipt findReceiptById(Long id);
     List<Receipt> findReceiptByStaffAndReceiptStatusOrderByDateDesc(Staff staff, ReceiptStatus receiptStatus);
-
-
-
-
-
+    List<Receipt> findAllByDateBetween(LocalDateTime dateStart, LocalDateTime dateEnd);
 
 }
--- src/main/java/com/example/util/ProductType.java
+++ src/main/java/com/example/util/ProductType.java
@@ -6,5 +6,4 @@ public enum ProductType {
     SOFTDRINK,
     SMOOTHIE,
     FOOD
-
 }
